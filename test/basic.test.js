f957a3d1dd5f675dc4158941be6b8d4bc9618330
natashta <natashta@local>
2019-04-02 21:36:15 +0300

    task done

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,7 +1,34 @@
-import sum from '../src/js/basic';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+import showHealth from '../src/js/app';
 
-  expect(result).toBe(6);
-});
+test('check critical', () => {
+    const input = {name: 'Маг', health: 10};
+    const expected = 'critical';
+    const received = showHealth(input);
+    
+    expect(received).toBe(expected);
+})
+
+test('check wounded', () => {
+    const input = {name: 'Маг', health: 50};
+    const expected = 'wounded';
+    const received = showHealth(input);
+    
+    expect(received).toBe(expected);
+})
+
+test('check healthy', () => {
+    const input = {name: 'Маг', health: 100};
+    const expected = 'healthy';
+    const received = showHealth(input);
+    
+    expect(received).toBe(expected);
+})
+
+test('check input', () => {
+    const input = {name: 'Маг', health: 'magitian'};
+    const expected = 'Ошибка. Значение может быть числом от 0 до 100';
+    const received = showHealth(input);
+    
+    expect(received).toBe(expected);
+})

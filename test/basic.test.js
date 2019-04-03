6d18a3a3c147c19fc5481ba4c160ee537dd5e56b
natashta <natashta@local>
2019-04-03 16:45:36 +0300

    appveyor changes

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,8 +1,7 @@
-
 import showHealth from '../src/js/app';
 
 test('check critical', () => {
-    const input = {name: 'Маг', health: 10};
+    const input = { name: 'Маг', health: 10 };
     const expected = 'critical';
     const received = showHealth(input);
     
@@ -10,7 +9,7 @@ test('check critical', () => {
 })
 
 test('check wounded', () => {
-    const input = {name: 'Маг', health: 50};
+    const input = { name: 'Маг', health: 50 };
     const expected = 'wounded';
     const received = showHealth(input);
     
@@ -18,7 +17,7 @@ test('check wounded', () => {
 })
 
 test('check healthy', () => {
-    const input = {name: 'Маг', health: 100};
+    const input = { name: 'Маг', health: 100 };
     const expected = 'healthy';
     const received = showHealth(input);
     
@@ -26,7 +25,7 @@ test('check healthy', () => {
 })
 
 test('check input', () => {
-    const input = {name: 'Маг', health: 'magitian'};
+    const input = { name: 'Маг', health: 'magitian' };
     const expected = 'Ошибка. Значение может быть числом от 0 до 100';
     const received = showHealth(input);
     
